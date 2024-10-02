57324b2f53651fb0b3ffcb29e9c68745ee2d421d
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-02 10:27:14 +0300

    fucked up big time

--- src/start.sh
+++ src/start.sh
@@ -5,8 +5,8 @@ TCMALLOC="$(ldconfig -p | grep -Po "libtcmalloc.so.\d" | head -n 1)"
 export LD_PRELOAD="${TCMALLOC}"
 
 # Check if /workspace/ComfyUI/models/insightface is mounted
-if mount | grep "/workspace/ComfyUI/models/insightface" > /dev/null; then
-    echo "/workspace/ComfyUI/models/insightface is mounted."
+if [ -d "/workspace/ComfyUI/models/insightface" ]; then
+    echo "/workspace/ComfyUI/models/insightface directory exists."
 
     # Create a soft link to /comfyui/models/insightface if it doesn't already exist
     if [ ! -L "/comfyui/models/insightface" ]; then
@@ -16,7 +16,7 @@ if mount | grep "/workspace/ComfyUI/models/insightface" > /dev/null; then
         echo "Soft link already exists."
     fi
 else
-    echo "/workspace/ComfyUI/models/insightface is not mounted."
+    echo "/workspace/ComfyUI/models/insightface directory does not exist."
 fi
 
 # Serve the API and don't shutdown the container
